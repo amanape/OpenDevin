4eca16e860c896ff2beda3d1dab49d503331ff20
amanape <83104063+amanape@users.noreply.github.com>
2024-06-07 17:42:03 +0300

    Update editor observations not to expect unneessary data

--- opendevin/events/observation/editor.py
+++ opendevin/events/observation/editor.py
@@ -4,17 +4,14 @@ from .observation import Observation
 
 @dataclass
 class CreateFileObservation(Observation):
-    path: str
-    success: bool
     observation = 'create'
     
     def __str__(self) -> str:
-        return f'**CreateFileObservation**\nsuccess: {self.success}\npath: {self.path}'
+        return f'**CreateFileObservation**\ncontent: {self.content}'
 
 
 @dataclass
 class ReadFileObservation(Observation):
-    path: str
     observation = 'read'
     
     def __str__(self) -> str:
@@ -23,9 +20,7 @@ class ReadFileObservation(Observation):
 
 @dataclass
 class UpdateFileObservation(Observation):
-    path: str
-    success: bool
     observation = 'update'
     
     def __str__(self) -> str:
-        return f'**UpdateFileObservation**\nsuccess: {self.success}\npath: {self.path}'
+        return f'**UpdateFileObservation**\ncode: {self.content}'
